c14b8c986b18eaf303ea9c4f7914bc29c795a87b
Isaac Shon <157827454+isaac-shon@users.noreply.github.com>
2024-10-13 18:54:52 -0400

    Add files via upload

---
+++ execute_code.py
@@ -0,0 +1,17 @@
+'''
+Runs all code in Mapping folder.
+'''
+import os
+os.chdir("C:\\Users\\isaac\\OneDrive\\Desktop\\example_codes\\Mapping\\")
+# Define the directory where the Python files are located
+code_directory = "C:\\Users\\isaac\\OneDrive\\Desktop\\example_codes\\Mapping\\code"
+
+# Loop through each file in the directory
+for filename in os.listdir(code_directory):
+    if filename.endswith(".py"):
+        file_path = os.path.join(code_directory, filename)
+        
+        with open(file_path, 'r') as file:
+            exec(file.read())
+        
+        print(f"Executed {filename}")
